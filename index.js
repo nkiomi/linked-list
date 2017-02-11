f7cd77a03571541c5eab4ee5ec48286cc6f32034
lucaskoala <lucasabreuoliveira@gmail.com>
2017-02-11 16:17:18 +0100

    function insert for linked list

--- index.js
+++ index.js
@@ -2,34 +2,67 @@
  * Object that represents a linkted list item
  *
  * @param {Number} key
- * @param {LListItem} prev
- * @param {LListItem} next
  * @constructor
  */
-var LListItem = function (key, prev, next) {
+var LListItem = function (key) {
 
     this.key = (key) ? key : 0;
-    this.prev = (prev) ? prev : null;
-    this.next = (next) ? next : null;
+    this.prev = null;
+    this.next = null;
 
 };
 
 /**
  * Object that represents a linked list
  *
- * @param {LListItem} firstItem
- * @param {LListItem} lastItem
+ * @param {string} name
  * @constructor
  */
-var LList = function (firstItem, lastItem) {
+var LList = function (name) {
 
-    this.firstItem = (firstItem) ? firstItem : null;
-    this.lastItem = (lastItem) ? lastItem : null;
+    this.name = (name) ? name : '';
+    this.firstItem = null;
+    this.lastItem = null;
 
 };
 
+/**
+ *
+ * @param {LList} list
+ * @param {LListItem} item
+ */
+function insert (list, item) {
 
+    if (!(list instanceof LList)) {
+        throw new Error('list param is not instance of LList');
+    }
 
+    if(!(item instanceof LListItem)){
+        throw new Error('item param is not instance of LListItem');
+    }
+
+    // empty list
+    if (!(list.lastItem)) {
+        list.firstItem = item;
+        list.lastItem = item;
+        return;
+    }
+
+    var oldLastItem = list.lastItem;
+
+    // non-empty list
+    oldLastItem.next = item;
+    item.prev = oldLastItem;
+    list.lastItem = item;
+
+}
+
+var listA = new LList('List A');
+var keysToInsert = [1,2,3,4,5,6,7];
+
+keysToInsert.forEach(function (key) {
+    insert(listA, new LListItem(key));
+});
 
 
 
